315512854bd4def97adf872939feaa9318dadf55
simwe <simonwestin@gmail.com>
2014-08-24 20:56:07 +0200

    added google account authentication with passport

--- ExpressApp1/app.js
+++ ExpressApp1/app.js
@@ -11,6 +11,7 @@ var path = require('path');
 
 var app = express();
 var exphbs = require('express-handlebars');
+var auth = require('./auth.js');
 
 
 //server.listen(3000);
@@ -24,14 +25,23 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+
+
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'keyboard cat' }));
+app.use(auth.passport.initialize());
+app.use(auth.passport.session());
+
 app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // development only
 if ('development' == app.get('env')) {
@@ -39,9 +49,41 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('home', { user: req.user });
      
 });
+
+app.get('/auth/google',
+    auth.passport.authenticate('google', { failureRedirect: '/' }),
+    function (req, res) {
+        res.redirect('/admin');
+});
+
+// GET /auth/google/return
+// Use passport.authenticate() as route middleware to authenticate the
+// request. If authentication fails, the user will be redirected back to the
+// login page. Otherwise, the primary route function function will be called,
+// which, in this example, will redirect the user to the home page.
+app.get('/auth/google/return',
+    auth.passport.authenticate('google', { failureRedirect: '/' }),
+    function (req, res) {
+        res.redirect('/admin');
+    });
+
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+app.get('/admin', ensureAuthenticated, function (req, res) {
+    res.render('admin', { user: req.user });
+});
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/')
+}
+
 
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
@@ -54,3 +96,4 @@ io.on('connection', function(socket){
   console.log('socket io - connected');
     socket.emit('info', { msg: 'The world is round, there is no up or down.' });
 });
+
---
+++ ExpressApp1/auth.js
@@ -0,0 +1,54 @@
+﻿
+
+module.exports.passport = require('passport'), GoogleStrategy = require('passport-google').Strategy;
+
+
+var users = [
+    { id: 'https://www.google.com/accounts/o8/id?id=AItOawnTxjub2VwX91fKRfHrmUnRhRiLTixNEwY', username: 'simon', email: 'simonwestin@gmail.com' }
+  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+function findById(id, fn) {
+    for (var i = 0, len = users.length; i < len; i++) {
+        var user = users[i];
+        if (user.id === id.identifier) {
+            return fn(null, user);
+        }
+    }
+    return fn(null, null);
+}
+
+module.exports.passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+
+module.exports.passport.deserializeUser(function (id, done) {
+    console.log('DESERIaLIzING USER:'+id);
+    findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+
+// Use the GoogleStrategy within Passport.
+// Strategies in passport require a `validate` function, which accept
+// credentials (in this case, an OpenID identifier and profile), and invoke a
+// callback with a user object.
+module.exports.passport.use(new GoogleStrategy({
+    returnURL: 'http://localhost:'+ process.env.PORT + '/auth/google/return',
+    realm: 'http://localhost:' + process.env.PORT
+},
+function (identifier, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+        // To keep the example simple, the user's Google profile is returned to
+        // represent the logged-in user. In a typical application, you would want
+        // to associate the Google account with a user record in your database,
+        // and return that user instead.
+        profile.identifier = identifier;
+        console.log('profile id set to:' + identifier);
+        return done(null, profile);
+    });
+}
+));
